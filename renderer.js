b87d37526ca1e4585873338a3f5bdd36b9c7de3e
jaydee_raymaker <jonerik84@gmail.com>
2019-05-13 16:28:15 +0200

    first commit

--- renderer.js
+++ renderer.js
@@ -1,3 +1,20 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+let arr = [];
+let int;
+
+exports.number = (val) => {
+    arr.push(val);
+    int = arr.join('');
+    console.log(`arr: ${arr}`);
+    console.log(`int: ${int}`);
+    return int;
+};
+
+exports.clear = () => {
+    arr = [];
+    int = 0;
+    console.log(`Cleared. \n arr=${arr} \n int=${int}`);
+};
